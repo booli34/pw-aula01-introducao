c91a85a97042ab740c89728de8edf1e166bb5611
booli34 <luhparda34@outlook.com>
2021-03-19 23:40:43 +0000

    Aula 01 - Primeiro componente

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () =>{
+
+    const informacao = 'primeiro componente JSX'
+
+    return(
+    <>
+        <h2>Componente OlaMundo</h2>
+        
+        <p>{informacao}</p>
+    </>
+    )
+}
